f78c42b55454677255fd06c2ad6712d61184e9b3
kspace-trk <keigonakao.0825@gmail.com>
2024-03-09 21:21:57 +0900

    fetch time api

--- server/routes/api/test.get.ts
+++ server/routes/api/test.get.ts
@@ -1,7 +1,10 @@
-export default eventHandler((event) => {
+export default eventHandler(async (event) => {
   const tmp = process.versions.node
+  const fetchResponse = await $fetch('https://worldtimeapi.org/api/timezone/Asia/Tokyo')
+
   return {
     nodeVersion: tmp,
     currentDate: new Date().toISOString(),
+    fetchResponse: fetchResponse,
   };
 });
